706a2e88276c2ab6dce62b03e35a0653d3178873
Diego Peralta <diegonly@gmail.com>
2021-06-07 17:19:31 -0400

    mensoPort

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ class Server{
 
 constructor(){
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT  || 80;
 
     //httpServer
     this.server = http.createServer(this.app);
